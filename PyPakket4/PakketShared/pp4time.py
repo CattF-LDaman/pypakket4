06d9de0959a1d9b47037dc36aff5658f3df57bfa
Luwe Daman <luwedm@gmail.com>
2020-04-14 20:05:36 +0200

    Added extra information to metadata file

--- PyPakket4/PakketShared/pp4time.py
+++ PyPakket4/PakketShared/pp4time.py
@@ -10,3 +10,6 @@ def from_POSIX_timestamp(ts):
 
     return datetime.datetime.fromtimestamp(ts).strftime("%A %d/%m/%Y (DD/MM/YYYY) at %T")
 
+def current_timestamp():
+
+    return datetime.datetime.now().strftime("%A %d/%m/%Y (DD/MM/YY) at %T")
